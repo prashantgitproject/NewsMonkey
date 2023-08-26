f95efd51e0c2c85a7167613c0d9ac57ad32f9029
Prashant SIngh <prashant.sb4754@gmail.com>
2023-08-26 22:24:59 +0530

    Prev. next button with systematic code

--- src/components/News.js
+++ src/components/News.js
@@ -18,7 +18,7 @@ export default class News extends Component {
 
   capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-}
+  }
 
   constructor(props) {
     super(props);
@@ -32,7 +32,8 @@ export default class News extends Component {
   }
 
   async updateNews() {
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0154721cb533489ca2decf04f74e699d&page=${this.props.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}
+    &apiKey=0154721cb533489ca2decf04f74e699d&page=${this.props.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -69,40 +70,20 @@ export default class News extends Component {
             this.state.articles.map((element) => {
               return (
                 <div className="col-md-4" key={element.url}>
-                  <NewsItems
-                    title={element.title ? element.title.slice(0, 45) : ""}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 88)
-                        : ""
-                    }
-                    imageUrl={element.urlToImage}
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                  />
+                  <NewsItems title={element.title ? element.title.slice(0, 45) : ""} 
+                  description={element.description? element.description.slice(0, 88): ""}
+                  imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
                 </div>
               );
             })}
         </div>
         <div className="container d-flex justify-content-between">
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handlePrevClick}
-          >
-            &larr; Previous
-          </button>
-          <button
-            disabled={
-              this.state.page + 1 >
-              Math.ceil(this.state.totalResults / this.props.pageSize)
-            }
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handleNextClick}
-          >
+          <button disabled={this.state.page <= 1}
+            type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous
+            </button>
+          <button disabled={this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)}
+            type="button" className="btn btn-dark" onClick={this.handleNextClick}>
             Next &rarr;
           </button>
         </div>
